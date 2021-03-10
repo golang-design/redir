85782ed30a761c7add8fb22a891de811b875cbf1
yangwenmai <yangwen.yw@gmail.com>
2021-03-11 01:48:41 +0800

    model: migrations with collink and visit sql scripts

---
+++ model/migrations/visit.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `visit` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `alias` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `kind` tinyint(1) NOT NULL DEFAULT '0',
+    `ip` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `ua` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `referer` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime NOT NULL,
+    `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
