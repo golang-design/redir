8e767e86a0845d2759510b173384382b4f9bc11d
yangwenmai <yangwen.yw@gmail.com>
2021-03-11 01:46:57 +0800

    utils: update

--- utils.go
+++ utils.go
@@ -1,34 +1,21 @@
-// Copyright 2020 The golang.design Initiative Authors.
-// All rights reserved. Use of this source code is governed
-// by a MIT license that can be found in the LICENSE file.
+// Copyright 2021 Changkun Ou. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
 
 package main
 
 import (
 	"math/rand"
+	"sync"
 	"time"
-	"unsafe"
 )
-
-// BytesToString converts byte slice to string.
-func BytesToString(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
-}
-
-// StringToBytes converts string to byte slice.
-func StringToBytes(s string) []byte {
-	return *(*[]byte)(unsafe.Pointer(&struct {
-		string
-		Cap int
-	}{s, len(s)}))
-}
 
 const alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+var r = newRand()
 
-// RandomString generates a random string
-func RandomString(n int) string {
+// randstr generates a random string
+func randstr(n int) string {
 	var str string
 	length := len(alphanum)
 	for i := 0; i < n; i++ {
@@ -36,4 +23,39 @@ func RandomString(n int) string {
 		str += string(a)
 	}
 	return str
+}
+
+type poolSource struct {
+	p *sync.Pool
+}
+
+func (s *poolSource) Int63() int64 {
+	v := s.p.Get()
+	defer s.p.Put(v)
+	return v.(rand.Source).Int63()
+}
+
+func (s *poolSource) Seed(seed int64) {
+	v := s.p.Get()
+	defer s.p.Put(v)
+	v.(rand.Source).Seed(seed)
+}
+
+func (s *poolSource) Uint64() uint64 {
+	v := s.p.Get()
+	defer s.p.Put(v)
+	return v.(rand.Source64).Uint64()
+}
+
+func newPoolSource() *poolSource {
+	s := &poolSource{}
+	p := &sync.Pool{New: func() interface{} {
+		return rand.NewSource(time.Now().Unix())
+	}}
+	s.p = p
+	return s
+}
+
+func newRand() *rand.Rand {
+	return rand.New(newPoolSource())
 }
