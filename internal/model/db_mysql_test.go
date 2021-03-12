97739d68002780f4c64fda7f8a0ee8562594882d
yangwenmai <yangwen.yw@gmail.com>
2021-03-13 07:24:02 +0800

    model: remove testify

--- internal/model/db_mysql_test.go
+++ internal/model/db_mysql_test.go
@@ -12,17 +12,20 @@ import (
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/stretchr/testify/require"
 )
 
 var dsn = "root:dc@tcp(127.0.0.1:3306)/" + dbname + "?charset=utf8mb4&parseTime=true"
 
 func TestStoreAlias(t *testing.T) {
 	mySQLDB, err := NewDB(dsn)
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatalf("NewDB with err: %v", err)
+	}
 	ctx := context.Background()
 	_, err = mySQLDB.sqlxDB.ExecContext(ctx, `truncate table collink`)
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatalf("truncate table collink with err: %v", err)
+	}
 	alias := "T1"
 	redirURL := "https://golang.design/s/talkgo"
 	red := &Redirect{
@@ -33,30 +36,52 @@ func TestStoreAlias(t *testing.T) {
 	}
 
 	err = mySQLDB.StoreAlias(ctx, red)
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatalf("StoreAlias with err: %v", err)
+	}
 	ret, err := mySQLDB.FetchAlias(ctx, alias)
-	require.NoError(t, err)
-	require.EqualValues(t, redirURL, ret.URL)
+	if err != nil {
+		t.Fatalf("FetchAlias with err: %v", err)
+	}
+	if ret.URL != redirURL {
+		t.Fatalf("wrong alias URL, want %s, got %v", redirURL, ret.URL)
+	}
 	ret.URL = "https://golang.design/s/go-questions"
 	ret.Kind = 1
 	err = mySQLDB.UpdateAlias(ctx, ret)
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatalf("UpdateAlias with err: %v", err)
+	}
 	ret, err = mySQLDB.FetchAlias(ctx, alias)
-	require.NoError(t, err)
-	require.NotEqualValues(t, redirURL, ret.URL)
+	if err != nil {
+		t.Fatalf("FetchAlias with err: %v", err)
+	}
+	if ret.URL == redirURL {
+		t.Fatalf("FetchAlias URL must be not equal, but got %v", ret.URL)
+	}
 	err = mySQLDB.DeleteAlias(ctx, alias)
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatalf("DeleteAlias with err: %v", err)
+	}
 	ret, err = mySQLDB.FetchAlias(ctx, alias)
-	require.Error(t, err)
-	require.Nil(t, ret)
+	if err == nil {
+		t.Fatalf("FetchAlias with err: %v", err)
+	}
+	if ret != nil {
+		t.Fatalf("FetchAlias return alias must be nil, got %+v", ret)
+	}
 }
 
 func TestVisit(t *testing.T) {
 	mySQLDB, err := NewDB(dsn)
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatalf("NewDB with err: %v", err)
+	}
 	ctx := context.Background()
 	_, err = mySQLDB.sqlxDB.ExecContext(ctx, `truncate table visit`)
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatalf("truncate table visit with err: %v", err)
+	}
 
 	now := time.Now()
 	visits := []*Visit{
@@ -70,27 +95,53 @@ func TestVisit(t *testing.T) {
 
 	for _, v := range visits {
 		err = mySQLDB.RecordVisit(ctx, v)
-		require.NoError(t, err)
+		if err != nil {
+			t.Fatalf("RecordVisit with err: %v", err)
+		}
 	}
 	ret, err := mySQLDB.CountReferer(ctx, "t2", 1, now.Add(-1*time.Second), now.Add(1*time.Second))
-	require.NoError(t, err)
-	require.Len(t, ret, 1)
+	if err != nil {
+		t.Fatalf("CountReferer with err: %v", err)
+	}
+	if len(ret) != 1 {
+		t.Fatalf("CountReferer len is 1, but got: %d", len(ret))
+	}
 
 	uast, err := mySQLDB.CountUA(ctx, "t2", 1, now.Add(-1*time.Second), now.Add(1*time.Second))
-	require.NoError(t, err)
-	require.Len(t, uast, 1)
+	if err != nil {
+		t.Fatalf("CountUA with err: %v", err)
+	}
+	if len(uast) != 1 {
+		t.Fatalf("CountUA result is not equal 1, got: %v", len(uast))
+	}
 
 	cvhst, err := mySQLDB.CountVisitHist(ctx, "t2", 1, now.Add(-10*time.Second), now.Add(1*time.Second))
-	require.NoError(t, err)
-	require.Len(t, cvhst, 1)
-	require.EqualValues(t, 1, cvhst[0].Count)
+	if err != nil {
+		t.Fatalf("CountVisitHist with err: %v", err)
+	}
+	if len(cvhst) != 1 {
+		t.Fatalf("CountUA result is not equal 1, got: %v", len(cvhst))
+	}
+	if cvhst[0].Count != 1 {
+		t.Fatalf("CountUA result want 1, got: %v", cvhst[0].Count)
+	}
 
 	cvhst, err = mySQLDB.CountVisitHist(ctx, "t3", 1, now.Add(-10*time.Second), now.Add(1*time.Second))
-	require.NoError(t, err)
-	require.Len(t, cvhst, 1)
-	require.EqualValues(t, 2, cvhst[0].Count)
+	if err != nil {
+		t.Fatalf("CountVisitHist with err: %v", err)
+	}
+	if len(cvhst) != 1 {
+		t.Fatalf("CountVisitHist result is not equal 1, got: %v", len(cvhst))
+	}
+	if cvhst[0].Count != 2 {
+		t.Fatalf("CountVisitHist result want 1, got: %v", cvhst[0].Count)
+	}
 
 	rs, err := mySQLDB.CountVisit(ctx, 1)
-	require.NoError(t, err)
-	require.Len(t, rs, 3)
+	if err != nil {
+		t.Fatalf("CountVisit with err: %v", err)
+	}
+	if len(rs) != 3 {
+		t.Fatalf("CountVisit result is not equal 3, got: %v", len(rs))
+	}
 }
